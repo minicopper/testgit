a65961296a5a4debf3343adce791c12fdf53c942
yuekui <yuekui001@ke.com>
2019-07-22 16:39:39 +0800

    conflict:resolve cherry-pick

--- index2.js
+++ index2.js
@@ -1,4 +1,10 @@
-let b=[1,2,3,4]
-function fun2(){
+let b = [1, 2, 3, 4]
+function fun2() {
   console.log(b)
 }
+
+let a = [1, 2, 3, 4]
+console.log(a)
+
+let c = [1, 2, 3, 4]
+console.log(a + b + c)
