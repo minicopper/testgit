8e48cfc667cd5478e1e063a4aaa3e05af0e58175
yuekui <yuekui001@ke.com>
2019-09-04 11:46:52 +0800

    fix:revert

--- index2.js
+++ index2.js
@@ -1,10 +1,15 @@
-let b = [1, 2, 3, 4]
-function fun2() {
+let b=[1,2,3,4]
+function fun2(){
   console.log(b)
 }
 
+<<<<<<< HEAD
 let a = [1, 2, 3, 4]
 console.log(a+b)
 
 let c = [1, 2, 3, 4]
 console.log(a + b + c + c+1)
+=======
+let a=[1,2,3,4]
+console.log(a)
+>>>>>>> parent of 78a4772... fix:add c
